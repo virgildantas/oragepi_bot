2df847f7b5b686154fe9d787f7fa1731991d2ff5
virgildantas <virgiliodantas.ti@gmail.com>
2017-05-09 00:48:52 -0300

    Update bot_telegram.watchdog.sh

--- bot_telegram.watchdog.sh
+++ bot_telegram.watchdog.sh
@@ -2,6 +2,6 @@ MXEASY=$(ps -A | grep -w bot_telegram.rb)
 
 if ! [ -n "$MXEASY" ] ; then
     cd /opt/orangepi_bot
-    kill -9 $(cat bot_telegram.pid
+    kill -9 $(cat bot_telegram.pid)
     /usr/local/rvm/gems/ruby-2.4.0@bot_telegram/wrappers/ruby bot_telegram.rb &
 fi
